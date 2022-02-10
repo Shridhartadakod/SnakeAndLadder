f5168d92bc08fbcb87fd042fedd2e8ab54ec34c4
Shridhartadakod <shridhar1491998@gmail.com>
2022-02-10 17:23:59 +0530

    Added Position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -12,7 +12,7 @@ namespace SnakeAndLadder
         {
             Console.WriteLine("Welcome to snake and ladder game");
             Console.ReadKey();
-
+            qentry.getMi
         }
     }
 }
